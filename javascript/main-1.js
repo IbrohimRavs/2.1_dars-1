5667fb4bfb65a9f9a34b1793e678ea254af85e0b
IbrohimRavshanov <ibrohimravshanov55@gmail.com>
2023-02-09 13:08:45 +0300

    first

---
+++ javascript/main-1.js
@@ -0,0 +1,15 @@
+let elinput = document.querySelector('.moor')
+
+
+
+elinput.addEventListener('change', function () {
+   body.classList.toggle('open')
+})
+
+let Name = document.querySelector('body')
+let button = document.querySelector('button')
+
+button.addEventListener('click', function () {
+   Name.classList.add('qora')
+
+})
